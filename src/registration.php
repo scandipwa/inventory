5d693b1ff3ffa1b03df3f09716f5ba7816a8f8ef
Alfreds Genkins <alfreds@scandipwa.com>
2021-02-09 17:27:43 +0200

    MSI implementation

---
+++ src/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category    ScandiPWA
+ * @package     ScandiPWA_Inventory
+ * @author      Alfreds Genkins <info@scandiweb.com>
+ * @copyright   Copyright (c) 2019 Scandiweb, Ltd (https://scandiweb.com)
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ScandiPWA_Inventory',
+    __DIR__
+);
